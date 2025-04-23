55d324e4d09fd87c96839a639e3bed0302a1b638
ganesh08052000 <ganeshsankar.svgh@gmail.com>
2025-04-23 16:02:02 +0530

    Add files via upload

---
+++ Git task/Javascript.js/introduction.js
@@ -0,0 +1,349 @@
+/*
+
+function ane()
+{
+    console.log('hhvghvh')
+}
+
+function aee_1()
+{
+console.log('hvvnbnb')
+}
+
+
+function aka()
+{ var a = 10
+var b=20
+console.log(a+b)
+}
+
+function Acttt()
+{
+    var faactor= "harii"
+    var mmovie= "cmhhv"
+    var songg = "hvchv"
+    console.log("favourite actor:" + faactor)
+}
+
+
+
+//var a =boolean
+//console.log(a)
+
+function areaa(lengh,ber)
+{
+    console.log(lengh*ber)
+}
+//areaa(12,121)
+
+function add (a,b)
+{
+    return a+b
+}
+  varrt total = add(10,20)
+  //console.log(total)
+/*
+  var ty = 50
+  if (ty>40)
+  {
+    console.log("correct")
+  }
+  else 
+  {
+    console.log("not correct")
+  }
+*/
+
+/*for (count=1;count<=5;count=count+1)
+{
+    console.log(count+'*2=',count*2)
+}
+*/
+
+
+/*farewell(); // Error: Cannot access 'farewell' before initialization
+const farewell = function() {
+  console.log("Goodbye");
+};
+*/
+
+
+
+/*
+
+let cde = {name:'hemu',heigh:222,sizee:67}
+
+console.log(cde['name'])
+
+
+var a = 12
+var b = a++
+console.log(a)
+
+var c=12
+var d = c++
+console.log(d)
+
+var e=12
+var f = ++e
+console.log(e)
+
+var g = 12
+var h = ++g
+console.log(h)
+
+
+let abv= [null,12121,'benz','dvj']
+console.log(abv[1])
+*/
+/*
+  let ad=1
+  while(ad<=9)
+  {
+    ad++
+    console.log(ad)
+
+
+  }
+*/
+/*
+let y = 0
+do
+{
+  while(y>5)
+}
+y++
+console.log(y)
+*/
+
+
+
+
+
+
+
+
+
+// const fruits = ["apple", "banana", "cherry"]
+// console.log(fruits)
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+// let i = 0;
+// while (i < 10) {
+//   if (i === 5) {
+//     break; // stops the loop when i is 5
+//   }
+//   console.log(i);
+//   i++;
+// }
+
+// // for (let i = 0; i < 5; i++) {
+// //   if (i === 2) {
+// //     continue; // skips the rest of this iteration when i is 2
+// //   }
+// //   console.log(i);
+// // }
+// // let yu =33
+// // switch () {
+// //   case value:
+    
+// //     break;
+
+// //   default:
+// //     break;
+// // }
+
+// // let tyt=0
+// // while(tyt<5)
+// // {
+// //   tyt++
+// //   console.log(tyt)
+ 
+// // }
+
+
+// let bn = 23
+// if(bn>12)
+// {
+//   console.log('ok')
+// }if(bn>20)
+// {
+//   console.log('good')
+// }
+// else{
+//   console.log('not ok')
+// }
+// let y=11
+// let f= 11
+// function n ()
+// {
+//   return f+y
+// }
+// var tot=f+y
+// console.log(tot)
+// n()
+
+
+// function ad (nam)
+// {
+//   return nam+2
+// }
+// function newfun(fn)
+// {
+//   console.log(fn(2))
+// }
+
+// newfun(ad)
+
+// function met (fnn)
+// {
+//   return fnn+233
+//   console.log(met(44))
+// }
+
+// console.log(met(44))
+
+// function callFunction(fn) {
+//   console.log(fn()); // calls the function passed in
+// }
+
+// callFunction(sayHello);
+
+// var t=12
+// console.log(t>10?"yes":'no')
+
+// var b=(s)=>
+// {
+//   return s*8
+// }
+// console.log(b(8))
+
+
+// var gh=[20,10,27,24,26]
+// console.log(gh.length)
+// var sum = 0
+// for (let i=0;i<5;i++)
+// {
+// sum=sum+gh[i]
+// }
+// console.log(sum)
+
+// var acc=2
+// const numbers = [1, 2, 3, 4];
+// const sum = numbers.reduce((acc, curr) => acc + curr, 3);
+// console.log(sum); // Output: 10
+
+// var mark=['vhjv','fgmbb',33,76,true,false,]
+// console.log(mark.sort().reverse());
+
+
+
+// for(f=0;f<mark.length;f++)
+
+
+// {
+//   mark[f]
+// }
+// console.log(mark)
+// console.log(mark.sort())
+
+
+// Today's Task
+
+//MAp
+let e=[12,14,16,1,20]
+console.log(e.map((f)=>f+20))
+console.log(e.map((f)=>f/20))
+
+// //Filter
+// const f =['a','b','c','d','e','f','g']
+// console.log(f.filter((fil)=> fil >'c'))
+
+// //reduce
+// var k=[12,1,13,14,15]
+// console.log(k.reduce((c,d)=>c*d))
+// console.log(k.reduce((c,d)=>c-d))
+
+
+// var bv ={}
+// bv.name='huxy'
+// bv.heig=5.6
+
+// console.log(bv)
+// console.log(name in bv)
+
+
+
+// let fgr=[44,56,32,12,66,1]
+// //console.log(fgr.sort((A,b)=>A-b))
+// console.log(fgr.sort().reverse().sort())
+//   var gh='bc'
+// function tyt() {
+//   var gh='hjhj'
+// }
+// console.log(gh)
+
+
+// var a=(j,k) => j+k
+// console.log(a(20,30))
+
+
+// var fg='dude'
+
+
+// if(1==1){
+//   var fg='machan'
+//   console.log(fg)
+// }
+
+// const A =10
+// var A = 2
+// console.log(A)
+
+// let gy =(k,l)=> console.log('dj')
+// gy()
+
+
+// var tytu= new Object()
+// tytu.name='hemu'
+// tytu.loc='chennai'
+// tytu.man='man'
+
+// for(let kud in tytu)
+// {
+//   console.log(tytu[kud])
+// }
+
+
+
+function teste1(a)
+{
+  a()
+  console.log('hi1')
+}
+
+
+
+function teste2(b)
+{
+  b()
+  console.log('hi2')
+}
+
+
+function teste3(c)
+{
+  c()
+  console.log('hi3')
+}
+
+
+function teste4(d)
+{
+  d()
+  console.log('hi4')
+}
+
+teste1(()=>{teste2(()=>{teste3(()=>{teste4(()=>{})})})})
